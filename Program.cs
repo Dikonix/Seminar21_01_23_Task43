55b705ff24c0c9718a8ea6afb1fd29e8e169faf7
Diana Brik <di.brik@yandex.kz>
2023-01-27 00:38:58 +0600

    Seminar 21.01.23 Task 43

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿void FindingIntersectionPoints(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2)
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k2 * x + b2;
+
+        Console.WriteLine($"Две прямые пересекутся в точке с координатами x: {x}, y: {y}");
+    }
+}
+
+Console.Write("Введите значение точки b1: ");
+double pointB1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение точки k1: ");
+double pointK1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение точки b2: ");
+double pointB2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение точки k2: ");
+double pointK2 = Convert.ToDouble(Console.ReadLine());
+
+FindingIntersectionPoints(pointB1, pointK1, pointB2, pointK2);
